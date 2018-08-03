2a087f91c6de9f252348b4d63810fc498222eb3f
cz <774666549@qq.com>
2018-08-03 18:49:48 +0800

    0

--- app/src/main/java/site/lool/android/competition/activity/level2/CaseHistoryActivity.java
+++ app/src/main/java/site/lool/android/competition/activity/level2/CaseHistoryActivity.java
@@ -5,46 +5,61 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import site.lool.android.competition.R;
-import site.lool.android.competition.activity.MainActivity;
+
+//region discription
+
+//endregion discription
 
 public class CaseHistoryActivity extends AppCompatActivity {
 
-    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
-        @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
-            String msg = "";
-            switch (menuItem.getItemId()) {
-                case R.id.action_upload:
-                    msg += "Click action_upload";
-                    break;
-                case R.id.action_date:
-                    msg += "Click action_date";
-                    break;
-            }
+    WebView webView;
 
-            if(!msg.equals("")) {
-                Toast.makeText(CaseHistoryActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-            return true;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setting();
+
+        fn();
+
+    }
+
+
+    //region 结构区
+    //设置
+    private void setting(){
+
+        //设置 主界面
         setContentView(R.layout.activity_case_history);
 
-        //ToolBar 处理
+        //设置 ToolBar
         Toolbar toolbar_caseHistroy = (Toolbar) findViewById(R.id.toolbar_caseHistroy);
         toolbar_caseHistroy.setTitle("病历查看");//标题
         setSupportActionBar(toolbar_caseHistroy);//启用点击响应
-        toolbar_caseHistroy.setOnMenuItemClickListener(onMenuItemClick);
+        toolbar_caseHistroy.setOnMenuItemClickListener(onMenuItemClickListener());//设置 toolbar 条目监听器
+        // onCreateOptionsMenu(Menu menu) //设置 初始化菜单 被调函数
+    }
+
+    //功能
+    private void fn(){
+
+        openURL();
+
+    }
+    //endregion discription
+
+    //region 实现区
+
+    //主视图中的 WebView 打开相应链接
+    private void openURL() {
 
         //获取前一个activity 提供的路径
         String host = this.getString(R.string.host);
@@ -62,16 +77,11 @@ public class CaseHistoryActivity extends AppCompatActivity {
         WebSettings settings = webView.getSettings();
         //启用js
         settings.setJavaScriptEnabled(true);
-
         //载入
         webView.loadUrl(url);
+        this.webView = webView;
+
     }
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.call_history,menu);
-        return true;
-    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -82,5 +92,51 @@ public class CaseHistoryActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //设置 - 控件 - toolbar 菜单项
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.call_history,menu);
+        return true;
+    }
+
+    //监听器 - toolbar 菜单项
+    private Toolbar.OnMenuItemClickListener onMenuItemClickListener(){
+        Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                String msg = ""+menuItem.getItemId();
+                switch (menuItem.getItemId()) {
+                    case R.id.action_by_year:
+                        msg += "Click action_upload";
+                        break;
+                    case R.id.action_date:
+                        msg += "Click action_date";
+                        break;
+                }
+
+                Toast.makeText(CaseHistoryActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        };
+        return onMenuItemClick;
+    }
+
+    //endregion
+
+    //region 响应区
+    public void upload(View view){
+        //打开相册
+
+        //上传数据
+        StringBuilder sb = new StringBuilder("");
+        sb.append(this.getString(R.string.host));
+        sb.append(this.getString(R.string.path_caseHistroy_photo));
+        String path_upload = sb.toString();
+        //调试信息
+        Toast.makeText(this,"上传位置："+path_upload,Toast.LENGTH_SHORT).show();
+    }
+    //endregion
 
 }
---
+++ app/src/test/java/site/lool/android/competition/Statement.java
@@ -0,0 +1,11 @@
+package site.lool.android.competition;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class Statement extends Activity{
+
+    {
+        Toast.makeText(this,"",Toast.LENGTH_SHORT).show();
+    }
+}
